2158d511a14114034a37318a10c8b1164ad82328
Manu Masson <manummasson8@gmail.com>
2016-02-18 20:17:57 +1100

    Fixed header

--- js/custom.js
+++ js/custom.js
@@ -42,7 +42,7 @@ jQuery(function($) {
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
             if (target.length) {
               $('html,body').animate({
-                scrollTop: target.offset().top - 40
+                scrollTop: target.offset().top - 9
               }, 900);
               return false;
             }
